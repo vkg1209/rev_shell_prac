da40fbf2c3605e9ce5154e1dcd2b1bb68217058b
vinay kumar <vinayg1209@gmail.com>
2026-01-07 17:49:32 +0530

    shell script

--- shell_script.sh
+++ shell_script.sh
@@ -15,6 +15,8 @@ if [ ! -d /var/log/$LOG_DIR_NAME ]; then
     mkdir /var/log/$LOG_DIR_NAME
 fi
 
+> $LOG_FILE
+
 if [ ! -f /var/log/$LOG_DIR_NAME/$LOG_FILE_NAME ]; then
     touch /var/log/$LOG_DIR_NAME/$LOG_FILE_NAME
 fi
