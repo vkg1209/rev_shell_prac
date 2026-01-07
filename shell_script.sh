76624715d1509f67c2828189a4b45c54490e0dfd
vinay kumar <vinayg1209@gmail.com>
2026-01-07 16:02:45 +0530

    shell script

--- shell_script.sh
+++ shell_script.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-START_TIME=$(date +%s)
-echo $START_TIME
-
-sleep 5
-sleep 2
-
-END_TIME=$(date +%s)
-echo $END_TIME
-
-TIME_TAKEN=$((END_TIME-START_TIME))
-echo $TIME_TAKEN Seconds
+echo $@
+echo $*
