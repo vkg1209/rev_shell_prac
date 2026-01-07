5134457aa0dc807f09511bf0ed9ee639267dc30d
vinay kumar <vinayg1209@gmail.com>
2026-01-07 16:20:07 +0530

    shell script

--- shell_script.sh
+++ shell_script.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-echo $@ : all variables
-echo $* : all variables
-echo $0 : script name
-echo $PWD : present working directory
-echo  $USER : current user
-echo $HOME : home directory of the user
-echo $$ : pid of this script
-sleep 5 &
-echo $! : pid of the last command in background
-echo $# : total number of arguments passed
-echo $LINENO : line number where error occured
-echo $BASH_COMMAND : displays the command of which the error occured
+WORLD_LEADERS=(MODI TRUMP PUTIN XIJINPING)
+echo ${WORLD_LEADERS[@]}
+
+WORLD_LEADERS=(MODI TRUMP PUTIN XI JINPING)
+echo ${WORLD_LEADERS[@]}
+
+WORLD_LEADERS=("MODI" "TRUMP" "PUTIN" "XI JINPING")
+echo ${WORLD_LEADERS[@]}
