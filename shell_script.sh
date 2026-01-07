ac4fc515f4ca3f8c4f8d961b8b3e0173c8f183c3
vinay kumar <vinayg1209@gmail.com>
2026-01-07 16:10:57 +0530

    shell script

--- shell_script.sh
+++ shell_script.sh
@@ -3,11 +3,11 @@
 echo $@ : all variables
 echo $* : all variables
 echo $0 : script name
-echo $pwd : present working directory
-echo  $user : current user
+echo $PWD : present working directory
+echo  $USER : current user
 echo $$ : pid of this script
 sleep 5 &
 echo $! : pid of the last command in background
 echo $# : total number of arguments passed
 echo $LINENO : line number where error occured
-echo $bash_command : displays the command of which the error occured
+echo $BASH_COMMAND : displays the command of which the error occured
