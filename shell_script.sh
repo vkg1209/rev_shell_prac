5775bd66b21f486820ad4e451bf574bccb61f4e3
vinay kumar <vinayg1209@gmail.com>
2026-01-07 17:37:34 +0530

    shell script

--- shell_script.sh
+++ shell_script.sh
@@ -10,6 +10,8 @@ LOG_DIR_NAME=shellscript-logs
 LOG_FILE_NAME=$0.log
 LOG_FILE=/var/log/$LOG_DIR_NAME/$LOG_FILE_NAME
 
+rm $LOG_FILE
+
 if [ ! -d /var/log/$LOG_DIR_NAME ]; then
     mkdir /var/log/$LOG_DIR_NAME
 fi
@@ -17,6 +19,7 @@ fi
 if [ ! -f /var/log/$LOG_DIR_NAME/$LOG_FILE_NAME ]; then
     touch /var/log/$LOG_DIR_NAME/$LOG_FILE_NAME
 fi
+
 
 check_root(){
 
