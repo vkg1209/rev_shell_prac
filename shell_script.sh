e0abe763b03e6f55f8111f69e848a5f2af8cff18
vinay kumar <vinayg1209@gmail.com>
2026-01-07 16:38:53 +0530

    shell script

--- shell_script.sh
+++ shell_script.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-USERNAME=vinay
+#checking user has a root access
+USER_ID=$(id -u)
 
-echo Enter Password $USERNAME: 
-read -s PASSWORD
+if [ USER_ID -ne 0 ]; then
+    echo "Please run this script with root privileges"
+    exit 1
 
-if [ $PASSWORD == sex ]; then
-    echo logged in successfully
-elif [ $PASSWORD == sexx ]; then
-    echo remove one x
-else 
-    echo wrong password
-fi
+dnf install mysql -y
+
+if [ $? -eq 0 ]; then
+    echo "mysql installed successfully : SUCCESS"
+else
+    echo "installing mysql is a failure: FAILURE"
+    exit 1
