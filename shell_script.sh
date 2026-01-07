412cb5948ffa52daff9d1de2f262918b01aebaba
vinay kumar <vinayg1209@gmail.com>
2026-01-07 16:29:39 +0530

    shell script

--- shell_script.sh
+++ shell_script.sh
@@ -5,9 +5,9 @@ USERNAME=vinay
 echo Enter Password $USERNAME: 
 read -s PASSWORD
 
-if ( $PASSWORD -eq sex ); then
+if [ $PASSWORD -eq sex ]; then
     echo logged in successfully
-elif ( $PASSWORD -ne sexx ); then
+elif [ $PASSWORD -ne sexx ]; then
     echo remove one x
 else 
     echo wrong password
