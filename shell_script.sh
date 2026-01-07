125c4f34c1e55dcfeb6cdb1744480b93ac608f2d
vinay kumar <vinayg1209@gmail.com>
2026-01-07 16:07:18 +0530

    shell script

--- shell_script.sh
+++ shell_script.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-echo $@
-echo $*
+echo $@ : all variables
+echo $* : all variables
+echo $0 : script name
+echo $pwd : present working directory
+echo  $user : current user
+echo $$ : pid of this script
+sleep 5 &
+echo $! : pid of the last command in background
+echo $# : total number of arguments passed
