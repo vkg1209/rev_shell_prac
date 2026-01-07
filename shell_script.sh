50180332bf5faf055c415739ee4be376dc19221c
vinay kumar <vinayg1209@gmail.com>
2026-01-07 17:12:49 +0530

    shell script

--- shell_script.sh
+++ shell_script.sh
@@ -31,7 +31,7 @@ check_root(){
 validate(){
     if [ $1 -ne 0 ]; then
         echo -e "Installing $2 :: $R FAILURE $N"
-        exit 1
+        # exit 1
     else
         echo -e "Installing $2 :: $G SUCCESS $N"
     fi
