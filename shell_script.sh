8aad6c420a049263c1c2e90586faff86606bb705
vinay kumar <vinayg1209@gmail.com>
2026-01-07 17:10:06 +0530

    shell script

--- shell_script.sh
+++ shell_script.sh
@@ -5,6 +5,18 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+#creating a log folder for this script
+LOG_DIR_NAME=shellscript-logs
+LOG_FILE_NAME=$0.log
+LOG_FILE=/var/log/$LOG_DIR_NAME/$LOG_FILE_NAME
+
+if [ ! -d /var/log/$LOG_DIR_NAME ]; then
+    mkdir /var/log/$LOG_DIR_NAME
+fi
+
+if [ ! -f /var/log/$LOG_DIR_NAME/$LOG_FILE_NAME ]; then
+    touch /var/log/$LOG_DIR_NAME/$LOG_FILE_NAME
+fi
 
 check_root(){
 
@@ -27,8 +39,11 @@ validate(){
 
 check_root
 
-dnf install mysql -y
+dnf install mysql -y &> $LOG_FILE
 validate $? mysql
 
-dnf install nginx -y
+dnf install dfdf &> $LOG_FILE
+validate $? dfdf
+
+dnf install nginx -y &> $LOG_FILE
 validate $? nginx
