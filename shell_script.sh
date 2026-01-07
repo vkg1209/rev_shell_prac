9200e9b645430b701efc50ec33c79b81aeb665b5
vinay kumar <vinayg1209@gmail.com>
2026-01-07 15:57:31 +0530

    shell script

--- shell_script.sh
+++ shell_script.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-export USERNAME="vinayy"
-unset USERNAME
-echo $USERNAME
+START_TIME=$(date +%s)
+
+sleep 5
+sleep 2
+
+END_TIME=$(date +%s)
+TIME_TAKEN=$((START_TIME-END_TIME))
+echo $TIME_TAKEN
