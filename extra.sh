8735bf8046fb05fe8687a744b7aa63f08a960d03
vinay kumar <vinayg1209@gmail.com>
2026-01-08 14:55:03 +0530

    shell practicce

--- extra.sh
+++ extra.sh
@@ -5,12 +5,18 @@
 # touch ../log1.log
 # touch ../log2.log
 # touch ../log3.log
-
+N=0
 ALL_LOG_PATHS=$(find /home/ec2-user -name "*.log")
 
 while IFS= read -r filepath 
 do
-    echo $filepath
+    $N=$(($N++))
+    $N > $filepath
     echo "-------------------------------------------------------------"
 done <<< $ALL_LOG_PATHS
+
+cat ../log1.log
+cat ../log2.log
+cat ../log3.log
+
 
