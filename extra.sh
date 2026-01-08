87a7d0f7ea0f24df8ccff19a34e0e41244e5f9b9
vinay kumar <vinayg1209@gmail.com>
2026-01-08 15:22:53 +0530

    shell practicce

--- extra.sh
+++ extra.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 
-# mkdir ../logs
+touch data.txt
+DATA=vinay
 
-# touch ../log1.log
-# touch ../log2.log
-# touch ../log3.log
-N=0
-ALL_LOG_PATHS=$(find /home/ec2-user -name "*.log")
+if [ -z data.txt ]; then
+    echo file is empty
+else
+    echo file is not empty
+fi
 
-while IFS= read -r filepath 
-do
-    $N=$(($N + 1))
-    $N > $filepath
-    echo "-------------------------------------------------------------"
-done <<< $ALL_LOG_PATHS
-
-cat ../log1.log
-cat ../log2.log
-cat ../log3.log
-
-
+if [ -z $DATA ]; then
+    echo Variable is empty
+else
+    echo Variable is not empty
+fi
