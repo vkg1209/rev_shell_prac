a15656f40d15914d7c56ea7cca94f58c61d234fe
vinay kumar <vinayg1209@gmail.com>
2026-01-08 13:01:34 +0530

    shell practicce

---
+++ script1.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+
+PASSWORD=this_is_script1_password
+
+
+echo this is script1
+
+echo running script2 now
+sh script2.sh
---
+++ script2.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+echo this is script2
+
+if [ PASSWORD == '' ]; then
+    echo we used sh so cant access the password variable from script1
+else
+    echo $PASSWORD
