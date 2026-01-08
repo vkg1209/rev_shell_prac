edaae7ee777892960a491c4c6f324d0f4530a859
vinay kumar <vinayg1209@gmail.com>
2026-01-08 13:07:03 +0530

    shell practicce

--- script2.sh
+++ script2.sh
@@ -1,11 +1,10 @@
 #!bin/bash
 
 echo this is script2
-
+trap 'echo we got error because it cant read password variable from scrip1' ERR
 if [ $PASSWORD = '' ]; then
     echo we used sh so cant access the password variable from script1
 else
     echo $PASSWORD
 fi
 
-trap 'echo we got error because it cant read password variable from scrip1' ERR
