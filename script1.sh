6dc151e15f2a1c38ac0639b8018f41dc7bd79f4b
vinay kumar <vinayg1209@gmail.com>
2026-01-08 13:11:02 +0530

    shell practicce

--- script1.sh
+++ script1.sh
@@ -6,4 +6,4 @@ PASSWORD=this_is_script1_password
 echo this is script1
 
 echo running script2 now
-sh script2.sh
+source script2.sh
--- script2.sh
+++ script2.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 echo this is script2
-trap 'echo we got error because it cant read password variable from scrip1' ERR
 if [ $PASSWORD = '' ]; then
     echo we used sh so cant access the password variable from script1
 else
