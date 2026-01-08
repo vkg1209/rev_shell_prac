baa6b71a80b356f6d463abe7e6e722382073f698
vinay kumar <vinayg1209@gmail.com>
2026-01-08 14:47:25 +0530

    shell practicce

--- extra.sh
+++ extra.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-mkdir ../logs
+# mkdir ../logs
 
-touch ../log1.log
-touch ../log2.log
-touch ../log3.log
+# touch ../log1.log
+# touch ../log2.log
+# touch ../log3.log
 
 ALL_LOG_PATHS=$(find /home/ec2-user -name *.log)
 
-while [ IFS= read -r filepath ]
+while IFS= read -r filepath 
 do
     echo $filepath
     echo "-------------------------------------------------------------"
